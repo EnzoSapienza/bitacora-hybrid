86d2bf2af3ff11d63c712f21a81b13fd3387af0d
EnzoSapienza <enzosapienzadev@gmail.com>
2026-06-14 21:25:33 -0300

    Todavía acomodando el flujo

---
+++ src/navigation/RootNavigator.tsx
@@ -0,0 +1,8 @@
+import useAuthStore from "../store/authStore";
+import AppNavigator from "./AppNavigator";
+import AuthNavigator from "./AuthNavigator";
+
+export default function RootNavigator() {
+  const isAuthenticated = useAuthStore((s) => s.isAuthenticated);
+  return isAuthenticated ? <AppNavigator /> : <AuthNavigator />;
+}
