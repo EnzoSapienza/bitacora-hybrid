dab28283f35ba1267a0a45cbb10d808938da39dc
EnzoSapienza <enzosapienzadev@gmail.com>
2026-06-15 22:55:39 -0300

    Bugfix: NavigatorContainer anidados

--- App.tsx
+++ App.tsx
@@ -1,36 +1,32 @@
-import { useEffect, useState } from 'react';
-import { NavigationContainer } from "@react-navigation/native";
+import { useEffect, useState } from "react";
 import { SafeAreaProvider } from "react-native-safe-area-context";
-import { authService } from './src/services/authService';
-import { useAuthStore } from './src/store/authStore';
+import { authService } from "./src/services/authService";
+import { useAuthStore } from "./src/store/authStore";
 import RootNavigator from "./src/navigation/RootNavigator";
 
 export default function App() {
+    const [loading, setLoading] = useState(true);
+    const setUser = useAuthStore((s) => s.setUser);
+    const clearUser = useAuthStore((s) => s.clearUser);
 
-  const [loading, setLoading] = useState(true);
-  const setUser = useAuthStore((s) => s.setUser);
-  const clearUser = useAuthStore((s) => s.clearUser);
+    useEffect(() => {
+        const unsubscribe = authService.subscribe((user) => {
+            if (user) {
+                setUser({ uid: user.uid, email: user.email! });
+            } else {
+                clearUser();
+            }
+            setLoading(false);
+        });
 
-  useEffect(() => {
-    const unsubscribe = authService.subscribe((user) => {
-      if (user) {
-        setUser({ uid: user.uid, email: user.email! });
-      } else {
-        clearUser();
-      }
-      setLoading(false);
-    });
+        return unsubscribe; // cleanup al desmontar
+    }, []);
 
-    return unsubscribe; // cleanup al desmontar
-  }, []);
+    if (loading) return null; // TODO: Splashscreen
 
-  if (loading) return null; // TODO: Splashscreen
-
-  return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <RootNavigator />
-      </NavigationContainer>
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <RootNavigator />
+        </SafeAreaProvider>
+    );
 }
